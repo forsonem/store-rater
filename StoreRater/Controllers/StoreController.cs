cc0dffc919345a81eba512cf6baf7a0f20b34fa1
forsonem <forsonem@gmail.com>
2020-02-10 14:01:17 -0600

    set up store index

--- StoreRater/Controllers/StoreController.cs
+++ StoreRater/Controllers/StoreController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StoreRater.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,11 @@ namespace StoreRater.Controllers
 {
     public class StoreController : Controller
     {
+        private StoreDbContext _db = new StoreDbContext();
         // GET: Store
         public ActionResult Index()
         {
-            return View();
+            return View(_db.Stores.ToList());
         }
     }
 }
