eed09cea3753e55c7aa5f4878636cfd63c702fe1
forsonem <forsonem@gmail.com>
2020-02-10 12:45:52 -0600

    Add project files.

---
+++ StoreRater/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StoreRater.Startup))]
+namespace StoreRater
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
