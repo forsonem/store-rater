b7c751588c8733944cff1d65fb00b33413e7d58a
forsonem <forsonem@gmail.com>
2020-02-10 13:12:35 -0600

    Set up my DbLayer

---
+++ StoreRater/Models/Store.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StoreRater.Models
+{
+    public class Store
+    {
+        public int StoreID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class StoreDbContext : DbContext
+    {
+        public DbSet<Store> Stores { get; set; }
+
+    }
+}
